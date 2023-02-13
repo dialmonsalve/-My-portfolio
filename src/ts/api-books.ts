31fd2ed654e7b0c4538477d9aa5fcb838fa4dbe7
Diego Alejandro <diales1981@gmail.com>
2023-02-12 19:45:12 -0500

    Finish Api books. Consulting from Google Books

---
+++ src/ts/api-books.ts
@@ -0,0 +1,156 @@
+import { IBooks, Item } from "./interfaces/IBooks";
+
+export class AstroBooks extends HTMLElement {	
+	resultado:Element
+	consult:string
+
+	constructor() {
+		super();
+
+		const search =this.querySelector("#search")
+		const form =this.querySelector("#form-search")
+
+		const cardId = this.querySelector('.card__img')
+		console.log(cardId)
+
+		cardId?.addEventListener('click', ()=>{
+			console.log("first")
+		})
+
+		this.consult = ""
+
+		this.resultado = document.querySelector(".grid-cards") as Element
+
+		if(search === null) return
+		if(form === null) return
+
+		search.addEventListener('change', ({target}:any) => {
+
+			if(target === null )return
+			this.consult = target.value
+	
+		});
+
+		form.addEventListener('submit', (e:Event)=>{
+			e.preventDefault()
+			this.searchData()
+		})
+
+	}
+	printAlert(message:string, type:string){	
+
+		this.clearHtml()
+		
+		const divMessage = document.createElement('DIV');
+		divMessage.classList.add('div-error');
+
+		if(type === 'error'){
+			divMessage.classList.add('alert-danger')
+		}
+		divMessage.textContent = message
+
+		document.querySelector('.grid-cards')?.insertBefore(divMessage, document.querySelector('.card'))
+
+		setTimeout(()=>{
+			divMessage.remove();
+		}, 2000)
+	}
+	async searchData  (){
+		const url = `https://www.googleapis.com/books/v1/volumes?q=${this.consult}`;
+
+			if(this.consult === ''){
+				this.printAlert('field search is required', 'error');
+				return;
+			}		
+
+		try {
+				const resp = await fetch(url);
+				const { items }:IBooks = await resp.json()
+								
+				this.showData(items)
+
+		} catch (error) {
+
+			if(error instanceof Error){
+				error.message
+			}
+		}
+	}
+	showData(items:Item[]){
+
+		this.clearHtml()
+
+		items.map(item => {
+			const {volumeInfo:{title, authors}, id} = item
+
+			let image = 'src/assets/no-image.svg'
+
+			const defineImage = (image:string)=> {
+
+				if(item.volumeInfo.imageLinks !== undefined){
+					return image = item.volumeInfo.imageLinks.thumbnail
+				}
+				return  image
+			}
+
+			const newTitle = () => {
+		
+				return title.length > 80
+					? title.substring(0, 80) + '...'
+					: title
+			};
+
+			this.resultado.innerHTML += `				
+				<div class="card">
+							<img class='card__img' src='${defineImage(image)}'/>
+							<h4 class='card__title'>
+								Title: ${ 
+									title === undefined
+										? "No title"
+										: newTitle()
+								}
+							</h4>
+
+							<p class='card__author'>
+								Author(s): ${
+									authors === undefined
+										? "No author(s)"
+										: authors
+							}</p>
+				</div>				`
+		});
+	}
+	clearHtml() {
+
+		if(this.resultado ){
+			while(this.resultado.firstChild){
+				this.resultado.removeChild(this.resultado.firstChild)
+			}
+		}
+	}
+	async getById  (){
+
+		const cardId = this.querySelector('.anchor')
+
+		cardId?.addEventListener('click', ()=>{
+			console.log("first")
+		})
+
+
+
+
+
+		
+		
+	}
+
+
+
+
+
+
+
+
+
+}
+customElements.define('astro-books', AstroBooks);
