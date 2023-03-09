08967fbbc6c0e6a468d29a5632ff913ee2a41796
Diego Monsalve <diales1981@gmail.com>
2023-03-08 19:52:18 -0500

    Add netlify file

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,8 @@
 import { defineConfig } from 'astro/config';
 
+import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
-import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 export default defineConfig({
